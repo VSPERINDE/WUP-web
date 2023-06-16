1feadee7d4d63899a05f3957f20ef0071a0cd1e5
VSPERINDE <56927912+VSPERINDE@users.noreply.github.com>
2023-06-16 17:23:26 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://workplace-use-pass-api.onrender.com",
+  baseURL: "http://localhost:8000",
 });
 
 export const createSession = (email, senha) =>
